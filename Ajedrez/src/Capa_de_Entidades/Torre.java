155784a6e88f348caa6d9267f3f2bae668da9c63
ramirojimenez094 <ramirojimenez094@gmail.com>
2015-09-22 17:15:45 -0300

    Método isMovimientoValido() realizado

--- Ajedrez/src/Capa_de_Entidades/Torre.java
+++ Ajedrez/src/Capa_de_Entidades/Torre.java
@@ -2,4 +2,12 @@ package Capa_de_Entidades;
 
 public class Torre  extends Pieza {
 	private static char nombre='t';
+	
+	public boolean isMovimientoValido (Posicion p){
+		boolean resp=false;
+		resp=((this.getPosicion().getNumero() == p.getNumero())||
+				(this.getPosicion().getLetra() == p.getLetra()));
+		if(this.getPosicion().equals(p)) resp=false;
+		return resp;
+	}
 }
