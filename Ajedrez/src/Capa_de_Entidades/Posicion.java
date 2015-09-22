e5bfff09396e12a22c0660028ba27321b0b44e59
ramirojimenez094 <ramirojimenez094@gmail.com>
2015-09-22 11:11:42 -0300

    Clase Posición creada

---
+++ Ajedrez/src/Capa_de_Entidades/Posicion.java
@@ -0,0 +1,20 @@
+package Capa_de_Entidades;
+
+public class Posicion {
+	private char letra;
+	private int numero;
+	public char getLetra() {
+		return letra;
+	}
+	public void setLetra(char letra) {
+		this.letra = letra;
+	}
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	
+	
+}
